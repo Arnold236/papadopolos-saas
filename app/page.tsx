e9343f23e9fb88cc2428f3a7021d72b302343615
Arnold236 <arnold@unitacsolution.co.za>
2025-08-27 11:26:54 +0200

    Companions and voice implemntation fully

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,39 @@ import CompanionCard from '@/components/CompanionCard'
 import CompanionsList from '@/components/CompanionsList'
 import CTA from '@/components/CTA'
 import { recentSessions } from '@/constants'
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.actions'
+import { getSubjectColor } from '@/lib/utils'
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({limit: 3});
+  const recentSessionsCompanions = await getRecentSessions(10);
   return (
       <main className="">
         <h1 className='text-2xl underline'>Popular Companions</h1>
-        <section className='home-section'>
+        {companions.map((companion) => (
+          <CompanionCard key={companion.id} {...companion} color={getSubjectColor(companion.subject)} />
+        ))}
+        {/* <section className='home-section'>
         <CompanionCard id={"123"} name ={"Maths is fun"} topic = {"Learn"} subject={"Maths"} duration={40} color= {"lightblue"}/>
         <CompanionCard id={"123"} name ={"Maths is fun"} topic = {"Learn"} subject={"Maths"} duration={40} color= {"pink"}/>
         <CompanionCard id={"123"} name ={"Maths is fun"} topic = {"Learn"} subject={"Maths"} duration={40} color= {"yellow"}/>
      
-        </section>
+        </section> */}
 
-        <section className='home-section'>
+        {/* <section className='home-section'>
           <CompanionsList 
             title="Recently Completed Sessions"
             companions={recentSessions}
+            classNames="w-2/3 max-lg:w-full"
+            />
+          <CTA/>
+          
+        </section> */}
+        <section className='home-section'>
+          <CompanionsList 
+            title="Recently Completed Sessions"
+            companions={recentSessionsCompanions}
             classNames="w-2/3 max-lg:w-full"
             />
           <CTA/>
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -18,3 +18,134 @@ export const createCompanion = async (formData : CreateCompanion ) => {
 
     return data[0];
 }
+
+export const getAllCompanions = async ({limit = 10, page = 1, subject, topic }: GetAllCompanions) => {
+    const superbase = createSupabaseClient();
+
+    let query = superbase.from( 'Companions').select();
+
+    if (subject && topic) {
+        query = query.ilike( 'subject',  `${subject}%`)
+            .or( `topc.ilike.%${topic},name.ilike.%${topic}%`)
+    }else if(subject){
+        query = query.ilike( 'subject', `${subject}%`)
+    }else if(topic){
+        query = query.or(`topc.ilike.%${topic},name.ilike.%${topic}%`)
+    }
+
+    query = query.range((page -1 ) * limit, page -1);
+
+    const {data: companions, error} = await query;
+
+    if (error) throw new Error(error.message);
+
+    return companions;
+}
+
+export const getCompanion = async (id: string) => {
+    const superbase = createSupabaseClient();
+
+    const {data,error } =  await superbase.from("Companions")
+        .select()
+        .eq( 'id', id);
+
+        if (error) {
+            return console.log(error);
+            
+        }
+        return data[0];
+}
+
+export const addToSessionHistory = async (companionId: string) =>
+{
+    const {userId} = await auth();
+
+    const superbase = createSupabaseClient();
+    const {data, error} = await superbase.from('session_history')
+        .insert({
+            companionId: companionId,
+            userId: userId
+        })
+
+    if (error) {
+        throw new Error(error.message)
+    }
+
+    return data;
+}
+
+
+export const getRecentSessions = async (limit = 10) => {
+    const superbase = createSupabaseClient();
+
+    const {data,error } =  await superbase.from("session_hisory")
+        .select(`companions: companion_id (*)`)
+        .order('created_at',{ascending: false})
+        .limit(limit);
+
+        if (error) {
+            return console.log(error.message);
+            
+        }
+        return data.map(({companions}) => companions);
+}
+
+export const getUserRecentSessions = async (userId:string, limit =10) => {
+    const superbase = createSupabaseClient();
+
+    const {data,error } =  await superbase.from("session_hisory")
+        .select(`companions: companion_id (*)`)
+        .eq('user_id', userId)
+        .order('created_at',{ascending: false})
+        .limit(limit);
+
+        if (error) {
+            return console.log(error.message);
+            
+        }
+        return data.map(({companions}) => companions);
+}
+
+export const getUserCompanions = async (userId:string) => {
+    const superbase = createSupabaseClient();
+
+    const {data,error } =  await superbase.from("companions")
+        .select()
+        .eq('author', userId)
+
+        if (error) {
+            return console.log(error.message);
+            
+        }
+        return data;
+}
+
+export const newCompanionPermissions = async () => {
+    const {userId, has} = await auth();
+    const superbase = createSupabaseClient();
+
+    let limit = 0;
+
+    if (has({plan: 'pro'})) {
+        return true;
+    } else if (has({feature: '10_conversations_month'})) {
+        limit = 3;
+    } else if (has({feature: '10_companion_limit'})) {
+        limit = 10;
+    }
+
+    const {data, error} = await superbase
+        .from('companions')
+        .select("id",{count: 'exact'} )
+        .eq("author", userId)
+
+        if (error) throw new Error(error.message)
+
+    const companionCount = data?.length;
+
+    if (companionCount >= limit) {
+        return false
+    }else{
+        return true;
+    }
+}
